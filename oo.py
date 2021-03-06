8e0e114e869c843907a2144a490da9f162bba176
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-03-06 04:17:58 +0000

    Complete Part 1: Classes and Init Methods. Define classes for Student, Question, and Exam.

--- oo.py
+++ oo.py
@@ -1,1 +1,25 @@
-# Create your classes here
+# --- define classes --- #
+
+class Student:
+    """ A student. """
+
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+
+class Question:
+    """ A question for an assessment. """
+
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+
+class Exam:
+    """ An entire exam. """
+
+    def __init__(self, name):
+        self.name = name
+        self.questions = [] # this will be a list of Question objects
